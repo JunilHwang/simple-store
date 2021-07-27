4520fa7e5909d7242d489ac41a984dc429b0007c
JunilHwang <junil.h@kakao.com>
2021-07-27 21:02:21 +0900

    refactor: 주석 추가

--- 05-simple-redux/src/core/Store.js
+++ 05-simple-redux/src/core/Store.js
@@ -2,19 +2,23 @@ import { observable } from './observer.js';
 
 export const createStore = (reducer) => {
 
+  // reducer가 실행될 때 반환하는 객체(state)를 observable로 만들어야 한다.
   const state = observable(reducer());
-  const frozenState = {};
 
+  // getState가 실제 state를 반환하는 것이 아니라 frozenState를 반환하도록 만들어야 한다.
+  const frozenState = {};
   Object.keys(state).forEach(key => {
     Object.defineProperty(frozenState, key, {
-      get: () => state[key]
+      get: () => state[key], // get만 정의하여 set을 하지 못하도록 만드는 것이다.
     })
-  })
+  });
 
+  // dispatch로만 state의 값을 변경할 수 있다.
   const dispatch = (action) => {
     const newState = reducer(state, action);
 
     for (const [key, value] of Object.entries(newState)) {
+      // state의 key가 아닐 경우 변경을 생략한다.
       if (!state[key]) continue;
       state[key] = value;
     }
@@ -22,6 +26,7 @@ export const createStore = (reducer) => {
 
   const getState = () => frozenState;
 
+  // subscribe는 observe로 대체한다.
   return { getState, dispatch };
 
 }
